67bc8b457075128e9b616f82fd64732dc9f3c997
aviniciusbb <aviniciusbb@hotmail.com>
2024-08-26 11:45:41 -0300

    content

--- Estimacao_MQO.R
+++ Estimacao_MQO.R
@@ -7,10 +7,14 @@
 library(tidyverse)
 
 
-# Abrir dados -----
+# -------------------------------------- #
+# Exemplo 1: Renda e gastos das familias #
+# -------------------------------------- #
+
+# Abrir dados 
 dados_gastos <- read_csv("dados/dados_gastos.csv")
 
-# Analisando os dados -----
+# Analisando os dados 
 glimpse(dados_gastos)
 
 
@@ -19,7 +23,7 @@ dados_gastos <- dados_gastos |>
   mutate(income = income * 100)
 
 
-# Visualizacao dos dados -----
+# Visualizacao - grafico de dispersao
 dados_gastos |> 
   ggplot() +
   geom_point(
@@ -31,6 +35,7 @@ dados_gastos |>
 cov(dados_gastos$food_exp, dados_gastos$income)
 cor(dados_gastos$food_exp, dados_gastos$income)
 
+
 # Correlacao - alternativo
 cov(dados_gastos$food_exp, dados_gastos$income) / 
   (sd(dados_gastos$food_exp) * sd(dados_gastos$income))
@@ -41,25 +46,33 @@ beta_1 <- cov(dados_gastos$food_exp, dados_gastos$income)/var(dados_gastos$incom
 beta_0 <- mean(dados_gastos$food_exp) - beta_1 * mean(dados_gastos$income)
 
 
-# Estimacao do modelo de MQO -----
+# Estimacao do modelo de MQO
 modelo <- lm(food_exp ~ income, data = dados_gastos) 
 summary(modelo)
 modelo$residuals
 
-# Adicionar reta de regressao -----
+# Adicionar reta de regressao 
 dados_gastos |> 
   ggplot() +
   geom_point(
     aes(x = income, y = food_exp)
     ) +
-  geom_smooth(
-    aes(x = income, y = food_exp),
-    color = "blue",
-    method = lm, se = FALSE
-  ) 
+  geom_abline(
+    intercept = beta_0,
+    slope = beta_1,
+    lwd = 2,
+    color = "green"
+  )
+# Alternativamente
+  #geom_smooth(
+  #  aes(x = income, y = food_exp),
+  #  color = "blue",
+  #  method = lm, se = FALSE
+  #) 
 
 
-# Observar residuos
+# Analise dos residuos 
+
 residuo_df <- data.frame(
   income = dados_gastos$income,
   residuo = modelo$residuals
@@ -78,8 +91,9 @@ residuo_df |>
     )
 
 
-# Distribuicao dos residuos
+# Distribuicao dos residuos 
 
+#* Histograma
 residuo_df |> 
   ggplot() +
   geom_histogram(
@@ -87,6 +101,7 @@ residuo_df |>
   ) +
   xlim(-400, 400)
 
+#* Densidade kernel
 residuo_df |> 
   ggplot() +
   geom_density(
@@ -94,21 +109,20 @@ residuo_df |>
   ) +
   xlim(-400, 400)
 
-# Media dos desvios
+# Estatisticas descritrivas dos desvios
+sum(residuo_df$residuo)
 mean(residuo_df$residuo)
+sd(residuo_df$residuo)
 
 
-# Coeficiente de determinacao
+# Coeficiente de determinacao R2 -----
 
 # R2 = 1 - (SQR/SQT)
 R2 <- 1 - sum(modelo$residuals^2) / 
   sum((dados_gastos$food_exp - mean(dados_gastos$food_exp))^2)
 
-# R2 = quadrado da correlacao
+# R2 = quadrado do coeficiente correlacao
 R2_alt <- cor(dados_gastos$food_exp, dados_gastos$income)^2
-
-
-
 
 
 
